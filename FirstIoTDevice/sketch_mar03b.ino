849c3848f421e7d5456853f9e2c2a8d7265042b1
Araz Aslanian <ava267@nyu.edu>
2019-04-18 12:29:40 +0400

    Update sketch_mar03b.ino

--- FirstIoTDevice/sketch_mar03b.ino
+++ FirstIoTDevice/sketch_mar03b.ino
@@ -1,78 +1,204 @@
- // Define hardware connections
+/*
+  WiFi UDP device responding to buttons on a web page
+  sent from a web page to node.js client to node.js server
+  and finally to here
+  code is at:
+  https://github.com/michaelshiloh/resourcesForClasses/tree/master/src/nodePrograms/udpAndHttpServer
+  created 3 February 2019
+  by Michael Shiloh
+  Change log:
+  14 Mar 2019 - ms -  better comments and cleaned up code
+                      Send binary data between Arduino and server
+*/
+
+#include <WiFiNINA.h>
+#include <WiFiUdp.h>
+
+int status = WL_IDLE_STATUS;
+#include "arduino_secrets.h"
+///////please enter your sensitive data in the Secret tab/arduino_secrets.h
+char ssid[] = SECRET_SSID;        // your network SSID (name)
+char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
+int keyIndex = 0;            // your network key Index number (needed only for WEP)
+
+unsigned int myPort = 5083;      // local port to listen on
+unsigned int remoteServerPort = 7083;  // remote port to talk to
+IPAddress remoteServerAddress(192, 168, 1, 13); 
+
+char packetBuffer[255]; //buffer to hold incoming packet
+
+WiFiUDP Udp;
+
+// Sensors
+//Define hardware connections
 #define PIN_GATE_IN 2
 #define IRQ_GATE_IN  0
 #define PIN_LED_OUT 13
 #define PIN_ANALOG_IN A0
 
-#include <LiquidCrystal.h>
-LiquidCrystal lcd(12,11,5,4,3,1);
+const int LED1 = 4;
+const int LED2 = 5;
 
-// soundISR()
-// This function is installed as an interrupt service routine for the pin
-// change interrupt.  When digital input 2 changes state, this routine
-// is called.
-// It queries the state of that pin, and sets the onboard LED to reflect that 
-// pin's state.
+int period = 1000;
+unsigned long time_now = 0;
+
+//// Actuators
+//const int NUMBER_OF_ACTUATORS = 1;
+//int actuators[NUMBER_OF_ACTUATORS] = {
+//  3, // pin numbers
+//  4,
+//  5
+//};
+
 void soundISR()
 {
   int pin_val;
 
   pin_val = digitalRead(PIN_GATE_IN);
-  digitalWrite(PIN_LED_OUT, pin_val);   
+  digitalWrite(PIN_LED_OUT, pin_val);
 }
 
-void setup()
-{
-  Serial.begin(9600);
-   lcd.begin(16, 2);
+
+String LED_ON_MESSAGE = "ledON";
+String LED_OFF_MESSAGE = "ledOFF";
+
+//// remember the button state so we only send
+//// when the state changes
+boolean ledState;
+boolean lastledState = LOW; // arbitrary
+
+void setup() {
+
+  //  // Set all actuator pins as outputs
+  //  for (int i = 0; i < NUMBER_OF_ACTUATORS; i++ ) {
+  //    pinMode(actuators[i], OUTPUT);
+  //  }
 
   //  Configure LED pin as output
   pinMode(PIN_LED_OUT, OUTPUT);
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
 
   // configure input to interrupt
   pinMode(PIN_GATE_IN, INPUT);
   attachInterrupt(IRQ_GATE_IN, soundISR, CHANGE);
 
-  // Display status
-  Serial.println("Initialized");
+  //Initialize serial
+  Serial.begin(9600);
+  // wait for serial port to connect. Needed for native USB port only
+  // Remove this if running without a serial monitor
+  while (!Serial) {
+    ;
+  }
+
+  // attempt to connect to WiFi network:
+  while ( status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    status = WiFi.begin(ssid, pass);
+
+    // wait 10 seconds for connection:
+    delay(10000);
+  }
+  Serial.println("Connected to wifi");
+  printWiFiStatus();
+
+  Serial.print("Initializing WiFiUDP library and listening on port ");
+  Serial.println(myPort);
+  Udp.begin(myPort);
 }
 
-void loop()
-{
+void loop() {
+
+  time_now = millis();
   int value;
 
-  // Check the envelope input
   value = analogRead(PIN_ANALOG_IN);
-  Serial.print(value);
 
-  // Convert envelope value into a message
   Serial.print("Status: ");
-  if(value <= 10)
-  {
+  if (value <= 10) {
     Serial.println("Quiet.");
-    lcd.clear();
+    digitalWrite(LED2, HIGH);
+    digitalWrite(LED1, LOW);
+    Serial.println("button state changed; sending new state");
+    Udp.beginPacket(remoteServerAddress, remoteServerPort);
+    Udp.write(9);
+    Udp.endPacket();
   }
-  else if( (value > 10) && ( value <= 30) )
-  {
+  else if ( (value > 10) && (value <= 30) ) {
     Serial.println("Moderate.");
-    lcd.clear();
-  }
-  else if(value > 30)
-  {
-    Serial.println("Loud.");
+    digitalWrite(LED2, HIGH);
+    digitalWrite(LED1, LOW);
+    Serial.println("button state changed; sending new state");
+    Udp.beginPacket(remoteServerAddress, remoteServerPort);
+    Udp.write(8);
+    Udp.endPacket();
     
-    lcd.setCursor(0,1);
-    lcd.clear();
-    lcd.print("Too loud!!!");
+  } else if (value > 30) {
+    Serial.println("Loud.");
+    digitalWrite(LED1, HIGH);
+    digitalWrite(LED2, LOW);
+    Serial.println("button state changed; sending new state");
+    Udp.beginPacket(remoteServerAddress, remoteServerPort);
+    Udp.write(1);
+    Udp.endPacket();
 
   }
+  while (millis() < time_now + period) {
 
-  // pause for 1 second
-  delay(1000);
-  
- 
-  // when blue button is pressed, start timer 1 and pause timer 2
- //when yellow button is pressed, start timer 2 and pause timer 1
- //when the off button is pressed, send time values to the server
+  }
+  // It's polite to listen first
+  // Did the server send us anything?
+  int packetSize = Udp.parsePacket();
+  if (packetSize)
+  {
+    Serial.print("Received packet of size ");
+    Serial.print(packetSize);
+    Serial.print(" from address ");
+    IPAddress remoteIp = Udp.remoteIP();
+    Serial.print(remoteIp);
+    Serial.print(", port ");
+    Serial.println(Udp.remotePort());
 
+    // read the packet into packetBufffer
+    int len = Udp.read(packetBuffer, 255);
+
+    //    // Activate the actuators as requested
+    //    digitalWrite(
+    //      actuators[(int)packetBuffer[0]],  // first byte is actuator number
+    //      (int)packetBuffer[1]);            // second byte is value
+    //  }
+
+    ledState = digitalRead(LED1);
+
+    if (ledState != lastledState) {
+
+      Serial.println("button state changed; sending new state");
+      Udp.beginPacket(remoteServerAddress, remoteServerPort);
+      Udp.write(ledState);
+      Udp.endPacket();
+
+      lastledState = ledState;
+    }
+  }
+
+  Serial.print(value);
+}
+
+void printWiFiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("My IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
 }
