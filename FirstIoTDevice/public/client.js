2c2a9be85a38b2be0239d82926da5958c8e62bf7
Araz Aslanian <ava267@nyu.edu>
2019-04-18 12:29:17 +0400

    Update client.js

--- FirstIoTDevice/public/client.js
+++ FirstIoTDevice/public/client.js
@@ -1,11 +1,59 @@
-function ledON() {
-    console.log ("sending a message to turn on the LED");
+/* 
+This is the websocket library which will allow us to send messages
+back to the web server 
+*/
+var socket = io();
+var messageonScreen = 'There is no black-and-white situation.';
+var counter = 0;
+
+socket.on('event', (data) => {
+	console.log('received data from webserver: ' + data);
+});
+
+socket.on('buttonPressed', (data) => {
+	console.log('received button pressed event from webserver: ' + data);
+	// document.body.style.backgroundColor = "red";
+	messageonScreen = 'You are being too loud, please calm down!!';
+	counter++;
+
+
+});
+
+socket.on('buttonReleased', (data) => {
+	console.log('received button released from webserver: ' + data);
+	// document.body.style.backgroundColor = "blue";
+	if (counter == 0) {
+		messageonScreen = 'There is no black-and-white situation.';
+    } else {
+		messageonScreen = 'Good job, keep  your arguing healthy';
+
+    }
+
+});
+
+function redLEDOn() {
+  socket.emit('redLEDOn'); 
 }
 
-function ledOFF() {
-    console.log ("sending a message to turn off the LED");
+function redLEDOff() {
+  socket.emit('redLEDOff');
 }
 
+function greenLEDOn() {
+  socket.emit('greenLEDOn'); 
+}
+
+function greenLEDOff() {
+  socket.emit('greenLEDOff');
+}
+
+function blueLEDOn() {
+  socket.emit('blueLEDOn'); 
+}
+
+function blueLEDOff() {
+  socket.emit('blueLEDOff');
+}
 
 let amt, startColor, newColor;
 var isWhite = true;
@@ -14,10 +62,12 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   
 
-  black = color(0,0,0)
-  white = color(255,255,255);
+  startColorText = color(0,0,0)
+  newColorText = color(255,255,255);
   startColor = color(255,255,255);
   newColor = color(0,0,0);
+  white = color(255,255,255);
+  black = color(0,0,0);
   amt = 0;
 
   background(startColor);
@@ -26,25 +76,25 @@ function setup() {
 
 function draw() {
   background(lerpColor(startColor, newColor, amt));
+  fill(lerpColor(startColorText,newColorText,amt));
   textAlign(CENTER);
-  text('There is no black-and-white situation.', windowWidth/2, 100);
-  amt += 0.006;
+  textSize(70);
+  text(messageonScreen, windowWidth/2, windowHeight/2);
+  amt += 0.002;
   if(amt >= 1){
     amt = 0.0;
     startColor = newColor;
+    startColorText = newColorText;
     if (isWhite) {
         newColor = black;
-        textColor = white;
+        newColorText = white;
         fill(255);
         isWhite = false;
     } else {
         newColor = white;
-        textColor = black;
+        newColorText = black;
         fill(0);
         isWhite = true;
     }
   }
 }
-
-//use d3.js to make a pie chart of time taken by each side at the end of the argument
-
